0e05c4926037f06d076d74b93cfff461b2a4fa88
Martin Burger <mburger@kochag.ch>
2022-09-30 10:51:31 +0200

    Add Time Variables and Time Task

--- U_Alarmclock_SolutionHS2022/main.c
+++ U_Alarmclock_SolutionHS2022/main.c
@@ -29,6 +29,15 @@
 extern void vApplicationIdleHook( void );
 
 //function forward declarations
+//Time in hours, minutes and seconds. Each as uint8_t to prevent corrupt data
+uint8_t hours = 18;
+uint8_t minutes = 15;
+uint8_t seconds = 00;
+
+uint8_t alarmHours = 21;
+uint8_t alarmMinutes = 35;
+uint8_t alarmSeconds = 00;
+
 void vApplicationIdleHook( void )
 {	
 	
@@ -38,6 +47,25 @@ int main(void)
 {
     vInitClock();
 	vInitDisplay();
+	xTaskCreate( vTimeTask, (const char *) "timetask", configMINIMAL_STACK_SIZE, NULL, 3, NULL); //Init TimeTask. Highest Priority to maximize Time accuracy
 	vTaskStartScheduler();
 	return 0;
+}
+
+void vTimeTask(void *pvParameters) {
+	for(;;) {
+		seconds++; //Increment Seconds
+		if(seconds >= 60) { //When seconds overflow, set to 0 and increment minutes
+			seconds = 0;
+			minutes++;
+		}
+		if(minutes >= 60) { //When minutes overflow, set to 0 and increment hours
+			minutes = 0;
+			hours++;
+		}
+		if(hours >= 24) { //When hours overflow, set to 0
+			hours = 0;
+		}
+		vTaskDelay(1000/portTICK_RATE_MS);
+	}
 }
